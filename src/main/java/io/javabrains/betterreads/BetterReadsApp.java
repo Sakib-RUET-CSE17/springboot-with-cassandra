abd42a6e3ddc346e7418f165b9bac080157f49db
sakibhasan17 <sakib.vivasoft@bkash.com>
2024-08-08 14:52:24 +0600

    initAuthors

--- src/main/java/io/javabrains/betterreads/BetterReadsApp.java
+++ src/main/java/io/javabrains/betterreads/BetterReadsApp.java
@@ -1,10 +1,17 @@
 package io.javabrains.betterreads;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import io.javabrains.betterreads.author.Author;
 import io.javabrains.betterreads.author.AuthorRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -26,34 +33,67 @@ public class BetterReadsApp {
     private AuthorRepository authorRepository;
 
     @Autowired
-    public void setRepo(AuthorRepository authorRepository){
+    public void setRepo(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(BetterReadsApp.class, args);
-	}
+    @Value("${datadump.location.author}")
+    private String authorDumpLocation;
+
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterReadsApp.class, args);
+    }
+
+    private void initAuthors() {
+        Path path = Paths.get(authorDumpLocation);
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.limit(10).forEach(line -> {
+                // Read & parse the line
+                String jsonString = line.substring(line.indexOf("{"));
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+
+                    // Construct Author object
+                    Author author = new Author();
+                    author.setName(jsonObject.optString("name"));
+                    author.setPersonalName(jsonObject.optString("personal_name"));
+                    author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+                    // Persist using Repo
+                    authorRepository.save(author);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initWorks() {
+
+    }
 
     @PostConstruct
-    public void start(){
-        System.out.println("BetterReadsApp saving author");
-        Author author = new Author();
-        author.setId("id");
-        author.setName("Name");
-        author.setPersonalName("personalName");
-        authorRepository.save(author);
+    public void start() {
+        System.out.println(authorDumpLocation);
+        initAuthors();
+        initWorks();
         System.out.println("BetterReadsApp saved author");
     }
 
     /**
-     * This is necessary to have the Spring Boot app use the Astra secure bundle 
+     * This is necessary to have the Spring Boot app use the Astra secure bundle
      * to connect to the database
      */
-	@Bean
+    @Bean
     public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
-	
+
 
 }
